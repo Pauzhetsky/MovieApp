cb7e4515bd6f0dd1b0bd6ba032ba7fc921130f8b
pauzhetsky <kazancev.alex2020@gmail.com>
2025-05-21 17:19:13 +0300

    all change

--- js/app.js
+++ js/app.js
@@ -1,52 +1,79 @@
 const API_KEY = '3445349c-5a9a-4a1c-a5a8-702fe5545dd6';
-const API_URL_TOP_250 =
-  'https://kinopoiskapiunofficial.tech/api/v2.2/films/collections?type=TOP_250_MOVIES&page=1';
+const API_URL_POPULAR =
+  'https://kinopoiskapiunofficial.tech/api/v2.2/films/top?type=TOP_100_POPULAR_FILMS&page=1';
+const API_URL_SEARCH =
+  'https://kinopoiskapiunofficial.tech/api/v2.1/films/search-by-keyword?keyword=';
 
-getMovies(API_URL_TOP_250);
+getMovies(API_URL_POPULAR);
 
 async function getMovies(url) {
-  const response = await fetch(url, {
+  const resp = await fetch(url, {
     headers: {
-      //   'content-type': 'application/json',
+      'Content-Type': 'application/json',
       'X-API-KEY': API_KEY
     }
   });
-  const responseData = await response.json();
-  showMovies(responseData);
+  const respData = await resp.json();
+  showMovies(respData);
+}
+
+function getClassByRate(vote) {
+  if (vote >= 7) {
+    return 'green';
+  } else if (vote > 5) {
+    return 'yellow';
+  } else {
+    return 'red';
+  }
 }
 
 function showMovies(data) {
-  const moviesElement = document.querySelector('.container__movies');
+  const moviesEl = document.querySelector('.movies');
 
-  data.items.forEach((movie) => {
-    const movieElement = document.createElement('div');
-    movieElement.classList.add('container__movies__movie');
-    movieElement.innerHTML = `<div class="container__movies__movie__cover-inner">
-            <img
-              src="${movie.posterUrlPreview}"
-              alt="${movie.nameRu}"
-              class="container__movies__movie__cover"
-            />
-            <div class="container__movies__movie__cover-darkened"></div>
-          </div>
-          <div class="container__movies__movie__info">
-            <div class="container__movies__movie__info__title">${
-              movie.nameRu
-            }</div>
-            <div class="container__movies__movie__info__category">
-              ${movie.genres.map((genre) => ` ${genre.genre}`).join('')}
-            </div>
-            <div class="container__movies__movie__info__average green">${
-              movie.ratingKinopoisk ? movie.ratingKinopoisk.toFixed(1) : 'Н/Д'
-            }</div>
-          </div>`;
+  // Очищаем предыдущие фильмы
+  document.querySelector('.movies').innerHTML = '';
 
-    function getRatingColor(rating) {
-      if (rating >= 4) return 'yellow';
-      if (rating >= 7) return 'green';
-      return 'red';
-    }
-
-    moviesElement.appendChild(movieElement);
+  data.films.forEach((movie) => {
+    const movieEl = document.createElement('div');
+    movieEl.classList.add('movie');
+    movieEl.innerHTML = `
+        <div class="movie__cover-inner">
+        <img
+          src="${movie.posterUrlPreview}"
+          class="movie__cover"
+          alt="${movie.nameRu}"
+        />
+        <div class="movie__cover--darkened"></div>
+      </div>
+      <div class="movie__info">
+        <div class="movie__title">${movie.nameRu}</div>
+        <div class="movie__category">${movie.genres.map(
+          (genre) => ` ${genre.genre}`
+        )}</div>
+        ${
+          movie.rating &&
+          `
+        <div class="movie__average movie__average--${getClassByRate(
+          movie.rating
+        )}">${movie.rating}</div>
+        `
+        }
+      </div>
+        `;
+    moviesEl.appendChild(movieEl);
   });
 }
+
+const form = document.querySelector('form');
+const search = document.querySelector('.header__search');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const apiSearchUrl = `${API_URL_SEARCH}${search.value}`;
+  if (search.value) {
+    getMovies(apiSearchUrl);
+
+    search.value = '';
+  }
+});
